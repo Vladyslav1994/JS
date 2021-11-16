164ac3e733e49d30b79b82d8fdc2bc25f42676a6
VladyslavBilokin <bilokin94@gmail.com>
2021-11-16 14:50:55 +0200

    ++

--- js/lesson3/homework3.js
+++ js/lesson3/homework3.js
@@ -64,7 +64,201 @@
 // - За допомогою циклу for і document.write() вивести 10 блоків div c довільним текстом і індексом всередині
 // - За допомогою циклу while вивести в документ 20 блоків h1 c довільним текстом всередині.
 // - За допомогою циклу while вивести в документ 20 блоків h1 c довільним текстом і індексом всередині.
-let mas = ['dsa','fasfsa'];
-for (let i = 0; i < 1; i = 0) {
-    document.write(`<div>${mas}</div>`);
-}
+
+// for (let i = 0; i < 1; i++) {
+//     document.write(`<div>me</div>`);
+// }
+// for (let i = 0; i < 1; i++) {
+//     document.write(`<div>you</div>`);
+// }
+// for (let i = 0; i < 1; i++) {
+//     document.write(`<div>they</div>`);
+// }
+// for (let i = 0; i < 1; i++) {
+//     document.write(`<div>us</div>`);
+// }
+// for (let i = 0; i < 1; i++) {
+//     document.write(`<div>hello</div>`);
+// }
+// for (let i = 0; i < 1; i++) {
+//     document.write(`<div>who</div>`);
+// }
+// for (let i = 0; i < 1; i++) {
+//     document.write(`<div>why</div>`);
+// }
+// for (let i = 0; i < 1; i++) {
+//     document.write(`<div>important</div>`);
+// }
+// for (let i = 0; i < 1; i++) {
+//     document.write(`<div>learn</div>`);
+// }
+// for (let i = 0; i < 1; i++) {
+//     document.write(`<div>it</div>`);
+// }
+// for (let i = 0; i < 1; i++) {
+//     document.write(`<div>me${1}</div>`);
+// }
+// for (let i = 0; i < 1; i++) {
+//     document.write(`<div>you${1}</div>`);
+// }
+// for (let i = 0; i < 1; i++) {
+//     document.write(`<div>they${1}</div>`);
+// }
+// for (let i = 0; i < 1; i++) {
+//     document.write(`<div>us${1}</div>`);
+// }
+// for (let i = 0; i < 1; i++) {
+//     document.write(`<div>hello${1}</div>`);
+// }
+// for (let i = 0; i < 1; i++) {
+//     document.write(`<div>who${1}</div>`);
+// }
+// for (let i = 0; i < 1; i++) {
+//     document.write(`<div>why${1}</div>`);
+// }
+// for (let i = 0; i < 1; i++) {
+//     document.write(`<div>important${1}</div>`);
+// }
+// for (let i = 0; i < 1; i++) {
+//     document.write(`<div>learn${1}</div>`);
+// }
+// for (let i = 0; i < 1; i++) {
+//     document.write(`<div>it${1}</div>`);
+// }
+// let qwe = ['123'];
+// let i = 0;
+// while (i < qwe.length) {
+//     document.write(`<h1>qwe[1]</h1>`);
+//     i++
+// }
+// let ewr = ['213'];
+// let i = 0;
+// while (i < ewr.length) {
+//     document.write(`<h2>yui[1]</h2>`);
+//     i++;
+// }
+// let ewg = ['213'];
+// let i = 0;
+// while (i < ewg.length) {
+//     document.write(`<h2>iuy[1]</h2>`);
+//     i++;
+// }
+// let ewh = ['213'];
+// let i = 0;
+// while (i < ewh.length) {
+//     document.write(`<h2>uyt[1]</h2>`);
+//     i++;
+// }
+// let ewb = ['213'];
+// let i = 0;
+// while (i < ewb.length) {
+//     document.write(`<h2>tyu[1]</h2>`);
+//     i++;
+// }
+// let ewj = ['213'];
+// let i = 0;
+// while (i < ewj.length) {
+//     document.write(`<h2>fgh[1]</h2>`);
+//     i++;
+// }
+// let ewk = ['213'];
+// let i = 0;
+// while (i < ewk.length) {
+//     document.write(`<h2>hgf[1]</h2>`);
+//     i++;
+// }
+// let ewm = ['213'];
+// let i = 0;
+// while (i < ewm.length) {
+//     document.write(`<h2>bvc[1]</h2>`);
+//     i++;
+// }
+// let ewn = ['213'];
+// let i = 0;
+// while (i < ewn.length) {
+//     document.write(`<h2>cvb[1]</h2>`);
+//     i++;
+// }
+// let ewo = ['213'];
+// let i = 0;
+// while (i < ewo.length) {
+//     document.write(`<h2>vcx[1]</h2>`);
+//     i++;
+// }
+// let ewp = ['213'];
+// let i = 0;
+// while (i < ewp.length) {
+//     document.write(`<h2>xcv[1]</h2>`);
+//     i++;
+// }
+// let xxx = ['213'];
+// let i = 0;
+// while (i < xxx.length) {
+//     document.write(`<h2>fds[1]</h2>`);
+//     i++;
+// }
+// let zzz = ['213'];
+// let i = 0;
+// while (i < zzz.length) {
+//     document.write(`<h2>sdf[1]</h2>`);
+//     i++;
+// }
+// let rrr = ['213'];
+// let i = 0;
+// while (i < rrr.length) {
+//     document.write(`<h2>rew[1]</h2>`);
+//     i++;
+// }
+// let TTT = ['213'];
+// let i = 0;
+// while (i < TTT.length) {
+//     document.write(`<h2>wer[1]</h2>`);
+//     i++;
+// }
+// let YYY = ['213'];
+// let i = 0;
+// while (i < YYY.length) {
+//     document.write(`<h2>cxz[1]</h2>`);
+//     i++;
+// }
+// let bbb = ['213'];
+// let i = 0;
+// while (i < bbb.length) {
+//     document.write(`<h2>zxc[1]</h2>`);
+//     i++;
+// }
+// let fff = ['213'];
+// let i = 0;
+// while (i < fff.length) {
+//     document.write(`<h2>asd[1]</h2>`);
+//     i++;
+// }
+// let ggg = ['213'];
+// let i = 0;
+// while (i < ggg.length) {
+//     document.write(`<h2>dsa[1]</h2>`);
+//     i++;
+// }
+
+// - Створити масив з 10 числових елементів. Вивести в консоль всі його елементи в циклі.
+// - Створити масив з 10 строкрових елементів. Вивести в консоль всі його елементи в циклі.
+// - Створити масив з 10 елементів будь-якого типу. Вивести в консоль всі його елементи в циклі.
+// - Створити масив з 10 елементів числового, стірчкового і булевого типу. За допомогою if та typeof вивести тільки булеві елементи
+// - Створити масив з 10 елементів числового, стірчкового і булевого типу. За допомогою if та typeof вивести тільки числові елементи
+// - Створити масив з 10 елементів числового, стрічкового і булевого типу. За допомогою if та typeof вивести тільки рядкові елементи
+
+// let numb = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// for (const p of numb) {
+//     document.write(`<div>`)
+//     document.write(p);
+//     document.write(`</div>`);
+// }
+
+
+// let str = ['h', 'e', 'l', 'l', 'o', 'o', 'k', 't', 'e', 'n'];
+// for (const z of str) {
+//     document.write(`<div>`)
+//     document.write(z);
+//     document.write(`</div>`);
+// }
+
