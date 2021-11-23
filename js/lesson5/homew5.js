637486d836e6cb5318062906dc81b146e7650854
VladyslavBilokin <bilokin94@gmail.com>
2021-11-23 11:58:53 +0200

    ++

---
+++ js/lesson5/homew5.js
@@ -0,0 +1,84 @@
+//- створити функцію яка обчислює та повертає площу прямокутника висотою
+
+// let xxx = (a, b) => a * b;
+// console.log(xxx(5,10));
+
+
+// - створити функцію яка обчислює та повертає площу кола
+
+// let xxx = (pi, r) => (pi * r) * 2;
+// console.log(xxx(3.14, 5));
+
+
+// - створити функцію яка обчислює та повертає площу циліндру
+
+// let xxx = (pi, R, H) => 2 * pi * R * H;
+// console.log(xxx(3.14, 3, 5));
+
+// - створити функцію яка приймає масив та виводить кожен його елемент
+
+// let array = (...xxx) => {
+//     console.log(xxx);
+// }
+// array(1,2,3,4);
+
+
+// - створити функцію яка  створює параграф з текстом. Текст задати через аргумент
+
+// function foorbar(fn) {
+//     fn();
+// }
+//
+// foorbar(() =>{
+//     document.write(`<p>hello</p>`);
+// })
+
+
+// - створити функцію яка  створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий
+
+// function foorbar(fn) {
+//     fn();
+// }
+//
+// foorbar(() =>{
+//     document.write(`<ul>`);
+//     document.write(`<li>hello</li>`);
+//     document.write(`<li>hello</li>`);
+//     document.write(`<li>hello</li>`);
+//     document.write(`</ul>`);
+// })
+
+
+// - створити функцію яка  створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий.
+// Кількість li визначається другим аргументом, який є числовим (тут використовувати цикл)
+
+// let xxx =(text,number) =>{
+//     document.write(`<ul>`)
+//     for (let i = 0; i < number; i++) {
+//         document.write(`<li>${text}</li>`);
+//     }
+//     document.write(`</ul>`);
+// }
+// xxx('hello', 3);
+
+
+// - створити функцію яка приймає масив примітивних елементів (числа,стрінги,булеві), та будує для них список
+// let other = [true, false, 10, 'hello'];
+//
+// let xxx = (number) => {
+//     for (const num of number) {
+//         document.write(`<div>${num}</div>`);
+//     }
+// };
+// xxx(other);
+
+
+// створити функцію яка приймає масив об'єктів з наступними полями id,name,age , та виводить їх в документ. Для кожного об'єкту окремий блок.
+// let docum = [{id: 1, name: 'vlad', age: 27}];
+//
+// let user = (other) => {
+//     for (let num of other) {
+//         document.write(`<div>${num.id} ${num.name} ${num.age}</div>`)
+//     }
+// };
+// user(docum)
