824f69a6b869720535feb05e914c2f5757c3c7ab
VladyslavBilokin <bilokin94@gmail.com>
2021-11-27 19:46:30 +0200

    ++

--- js/lesson6/cw6html.js
+++ js/lesson6/cw6html.js
@@ -7,6 +7,135 @@
 // let n2 = 'Ron Whisley'
 // let n3 = 'Hermione Granger'
 
-let users = (user) => {
 
+// let users = (user) => {
+//     let split = user.split('');
+//
+//     let badArr = '!@#$%^&*()_+-.,'.split('')
+//
+//     const arr = split.map((item, i) => {
+//         if (badArr.includes(item)) {
+//             return badArr.includes(split[i-1]) ? '' : ' '
+//         }
+//         return item;
+//     })
+//     return arr.join('')
+// };
+// console.log(users('Harry..Potter'));
+// console.log(users('Ron---Whisley'));
+// console.log(users('Hermione__Granger'));
+// console.log(users('Hermione__!@#$%^&*()__Granger'));
+
+
+// - створити функцію, яка генерує масив рандомних числових цілих значень в діапазоні від 0 до 100.
+
+// let arrey = () => {
+//     let number = [0];
+//     for (let i = 0; i < 100; i++) {
+//         // let a = Math.random() * 100 | 0;
+//         let a = Math.round(Math.random() * 100);
+//         number.push(a);
+//     }
+//     console.log(number)
+// }
+// arrey()
+
+
+// - створити (або згенерувати, за допомоги попередньої функції) масив рандомних цілих числових значень. Відсортувати його за допомоги sort
+// let arrey = () => {
+//     let number = [0];
+//     for (let i = 0; i < 100; i++) {
+//         // let a = Math.random() * 100 | 0;
+//         let a = Math.round(Math.random() * 100);
+//         number.push(a);
+//         let b = number.sort()*10;
+//         console.log(b);
+//     }
+//     console.log(number)
+// }
+// arrey()
+
+
+// - створити (або згенерувати, за допомоги попередньої функції) масив рандомних цілих числових значень.
+// відфільтрувати  його за допомоги filter, залишивши тільки парні числа
+
+// let arrey = () => {
+//         let number = [0];
+//         for (let i = 0; i < 100; i++) {
+//             // let a = Math.random() * 100 | 0;
+//             let a = Math.round(Math.random() * 100);
+//             number.push(a);
+//
+//         }
+//         console.log(number)
+//
+// }
+// arrey()
+
+
+// - створити функцію sortNums(direction), яка прймає масив чисел, та сортує його від більшого до меньшого, або навпаки
+// в залежності від значення аргументу direction.
+//     let nums = [11,21,3];
+// sortNums('ascending') // [3,11,21]
+// sortNums('descending') // [21,11,3]
+
+// let nums = [11,21,3];
+// let filter = (n, direction) => n.sort((a,b) => direction ? a-b : b-a);
+//
+// console.log(filter(nums, true));
+
+
+
+// - Знайти та вивести довижину настипних стрінгових значень
+// 'hello world', 'lorem ipsum', 'javascript is cool'
+// - Перевести до великого регістру наступні стрінгові значення
+// 'hello world', 'lorem ipsum', 'javascript is cool'
+// - Перевести до нижнього регістру настипні стрінгові значення
+// 'HELLO WORLD', 'LOREM IPSUM', 'JAVASCRIPT IS COOL'
+
+
+// - Напишіть функцію stringToarray(str), яка перетворює рядок на масив слів.
+//     let str = 'Каждый охотник желает знать';
+// let arr = stringToarray(str);
+// document.writeln(arr); // ['Каждый', 'охотник', 'желает', 'знать']
+
+// let array = (xxx) => {
+//     return xxx.split(" ")
+// };
+//
+// let qqq = 'Каждый охотник желает знать';
+// let sss = array(qqq);
+// console.log(sss);
+
+
+// - Напишіть функцію delete_characters(str, length), яка повертає підрядок, що складається із зазначеної кількості символів.
+//     let str = 'Каждый охотник желает знать';
+// document.writeln(delete_characters(str, 7)); // Каждый
+
+
+// let sss = (str, index) => {
+//     return str.substr(0,index)
+//
+// };
+// let str = 'Каждый охотник желает знать';
+// document.write(sss(str, 7));
+
+
+// - Напишіть функцію insert_dash(str), яка приймає рядок str як аргумент і вставляє тире (-) між словами. При цьому всі символи рядка необхідно перевести у верхній регістр.
+//     let str = "HTML JavaScript PHP";
+// document.writeln(insert_dash(str)); // 'HTML-JAVASCRIPT-PHP'
+
+// let str = "HTML JavaScript PHP";
+// let sss = (insert) => {
+//     return insert.replaceAll(' ', '-').toUpperCase();
+//
+// };
+// let aaa = sss(str);
+// console.log(aaa);
+
+let str = 'охотник';
+let xxx = (sss) => {
+    return sss.replace('о', 'О');
 };
+let qwe = xxx(str);
+console.log(qwe);
---
+++ js/lesson7/hm7js.js
@@ -0,0 +1,75 @@
+// - Створити функцію конструктор для об'єктів User з полями id, name, surname , email, phone
+// створити пустий масив, наповнити його 10 об'єктами new User(....)
+
+// function Users (usersid, usersname, userssurname, usersemail, usersphone) {
+//     this.id = usersid;
+//     this.name = usersname;
+//     this.surname = userssurname;
+//     this.email = usersemail;
+//     this.phone = usersphone;
+// }
+// let user = [
+//     new Users(123, 'Ania', 'Antonova', 'Ania@qwe.ua', 85564254546),
+//     new Users(42, 'Misha', 'Faraonov', 'mis@qwe.ua', 8566654788),
+//     new Users(351, 'Nastya', 'Afrimova', 'nast@qwe.ua', 856123188),
+//     new Users(423, 'Yuri', 'Anatski', 'yyrr@qwe.ua', 8123654788),
+//     new Users(52, 'Lili', 'Panova', 'li@qwe.ua', 85666124128),
+//     new Users(65, 'Anton', 'Markov', 'ant@qwe.ua', 8564214188),
+//     new Users(767, 'Maria', 'Franowa', 'mar@qwe.ua', 8531231228),
+//     new Users(843, 'Nik', 'Bondarenko', 'Nik@qwe.ua', 8564214188),
+//     new Users(92, 'Yaroslav', 'Panenko', 'yaro@qwe.ua', 856642141),
+//     new Users(10, 'Eva', 'Radchenko', 'eeva@qwe.ua', 8563213788)];
+// console.log(user)
+
+// - Взяти масив з  User[] з попереднього завдання, та відфільтрувати , залишивши тільки об'єкти з парними id (filter)
+
+
+// let filtr = user.filter((item) => {
+//     if (item.id % 2 === 0){
+//     return item
+//     }});
+// console.log(filtr);
+
+// - Взяти масив з  User[] з попереднього завдання, та відсортувати його по id. по зростанню (sort)
+
+// user.sort((a, b) => {
+//     return a.id - b.id;
+// });
+// console.log(user)
+
+
+// - створити класс для об'єктів Client з полями id, name, surname , email, phone, order (поле є масивом зі списком товарів)
+// створити пустий масив, наповнити його 10 об'єктами Client
+
+class Client {
+    constructor(id, name, surname , email, phone, order) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.email = email;
+        this.phone = phone;
+        this.order = order;
+
+    }
+}
+
+let client = [
+    new Client(123, 'Ania', 'Antonova', 'Ania@qwe.ua', 85564254546, [1,2,3,4,5,6,7]),
+    new Client(42, 'Misha', 'Faraonov', 'mis@qwe.ua', 8566654788,[1,2,3,4,5]),
+    new Client(351, 'Nastya', 'Afrimova', 'nast@qwe.ua', 856123188,[1,2,3,4,5,6]),
+    new Client(423, 'Yuri', 'Anatski', 'yyrr@qwe.ua', 8123654788,[1,2,3]),
+    new Client(52, 'Lili', 'Panova', 'li@qwe.ua', 85666124128,[1]),
+    new Client(65, 'Anton', 'Markov', 'ant@qwe.ua', 8564214188,[1,2,3,4,5,6,7,123,3]),
+    new Client(767, 'Maria', 'Franowa', 'mar@qwe.ua', 8531231228,[1,2]),
+    new Client(843, 'Nik', 'Bondarenko', 'Nik@qwe.ua', 8564214188,[1,2,3,4]),
+    new Client(92, 'Yaroslav', 'Panenko', 'yaro@qwe.ua', 856642141,[1,2,3,4,5,6,7,8]),
+    new Client(10, 'Eva', 'Radchenko', 'eeva@qwe.ua', 8563213788,[1,2,3,4,5,2,3,45,4])];
+
+console.log(client);
+
+// - Взяти масив (Client [] з попереднього завдання).Відсортувати його по кількості товарів в полі order по зростанню. (sort)
+
+client.sort((a, b) => {
+    return a.order.length - b.order.length;
+    });
+console.log(client)
