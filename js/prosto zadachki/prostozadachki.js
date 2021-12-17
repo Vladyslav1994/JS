ec2d83089baf805e51f093659d4249116a0d25cc
VladyslavBilokin <bilokin94@gmail.com>
2021-12-17 16:31:17 +0200

    ++

---
+++ js/prosto zadachki/prostozadachki.js
@@ -0,0 +1,19 @@
+// Задача 1. С помощью метода prompt() получите сначала имя пользователя, затем год его рождения и сохраните в 2 переменные.
+//     Вычислите возраст пользователя, и выведите его в абзаце с помощью document.write() в таком формате "Антон: 24".
+// let name = prompt('write name and surname');
+// let name2 = prompt('write name and surname');
+// let name3 = prompt('write name and surname');
+// document.write(`<ol>`)
+//     for (let i = 0; i < 1 ; i ++) {
+//         document.write(`<li>${name}</li>`)
+//         document.write(`<li>${name2}</li>`)
+//         document.write(`<li>${name3}</li>`)
+//     }
+// document.write(`</ol>`)
+//
+// document.write(`<ol>`)
+// for (let i = 0; i < 3 ; i ++) {
+//     let name = prompt('write name and surname');
+//     document.write(`<li>${name}</li>`)
+// }
+// document.write(`</ol>`)
