0df34a8e093ad51c34af421a27edff76017868b2
VladyslavBilokin <bilokin94@gmail.com>
2021-12-20 15:10:04 +0200

    ++

--- js/lesson12/cw12/cw12js.js
+++ js/lesson12/cw12/cw12js.js
@@ -3,62 +3,62 @@
 // https://jsonplaceholder.typicode.com/posts
 //     зробити кнопку до кожного поста. при кліку на яку виводяться в окремий блок всі коментарі поточного поста
 
-// fetch('https://jsonplaceholder.typicode.com/posts')
-//     .then((response) => response.json())
-//     .then((jsons) => {
-//         let wrap = document.createElement('div');
-//
-//         for (let post of jsons) {
-//             let div_postId = document.createElement('div');
-//             let div_id = document.createElement('div');
-//             let div_title = document.createElement('div');
-//             let div_body = document.createElement('div');
-//             let btn1 = document.createElement('button');
-//
-//             div_postId.innerText = `${post.userId}`;
-//             div_id.innerText = `${post.id}`;
-//             div_title.innerText = `${post.title}`;
-//             div_body.innerText = `${post.body}`;
-//             btn1.innerText = 'click';
-//
-//             wrap.classList.add('wraps')
-//             btn1.classList.add('btn1');
-//             div_id.classList.add('div_id1');
-//
-//
-//             btn1.onclick = () => {
-//                 fetch(`https://jsonplaceholder.typicode.com/posts/${post.id}/comments`)
-//                     .then((response) => response.json())
-//                     .then((jsons) => {
-//                         const oldWrap = document.getElementsByClassName('commentsWrapper')[0]
-//                         if(oldWrap) {
-//                             oldWrap.remove();
-//                         }
-//
-//                         let wrapComments = document.createElement('div');
-//                         wrapComments.classList.add('commentsWrapper');
-//
-//                         jsons.forEach(comment => {
-//                             let div_commentId = document.createElement('div');
-//                             let div_commentName = document.createElement('div');
-//
-//                             div_commentId.innerText = `${comment.id}`;
-//                             div_commentName.innerText = `${comment.name}`;
-//
-//                             wrapComments.append(div_commentId, div_commentName);
-//                         })
-//
-//                         document.body.append(wrapComments);
-//                     })
-//             }
-//             wrap.append(div_postId, div_id, div_title, div_body, btn1);
-//
-//         }
-//         document.body.append(wrap);
-//
-//
-//
-//     })
-// ;
+fetch('https://jsonplaceholder.typicode.com/posts')
+    .then((response) => response.json())
+    .then((jsons) => {
+        let wrap = document.createElement('div');
+
+        for (let post of jsons) {
+            let div_postId = document.createElement('div');
+            let div_id = document.createElement('div');
+            let div_title = document.createElement('div');
+            let div_body = document.createElement('div');
+            let btn1 = document.createElement('button');
+
+            div_postId.innerText = `${post.userId}`;
+            div_id.innerText = `${post.id}`;
+            div_title.innerText = `${post.title}`;
+            div_body.innerText = `${post.body}`;
+            btn1.innerText = 'click';
+
+            wrap.classList.add('wraps')
+            btn1.classList.add('btn1');
+            div_id.classList.add('div_id1');
+
+
+            btn1.onclick = () => {
+                fetch(`https://jsonplaceholder.typicode.com/posts/${post.id}/comments`)
+                    .then((response) => response.json())
+                    .then((jsons) => {
+                        const oldWrap = document.getElementsByClassName('commentsWrapper')[0]
+                        if(oldWrap) {
+                            oldWrap.remove();
+                        }
+
+                        let wrapComments = document.createElement('div');
+                        wrapComments.classList.add('commentsWrapper');
+
+                        jsons.forEach(comment => {
+                            let div_commentId = document.createElement('div');
+                            let div_commentName = document.createElement('div');
+
+                            div_commentId.innerText = `${comment.id}`;
+                            div_commentName.innerText = `${comment.name}`;
+
+                            wrapComments.append(div_commentId, div_commentName);
+                        })
+
+                        document.body.append(wrapComments);
+                    })
+            }
+            wrap.append(div_postId, div_id, div_title, div_body, btn1);
+
+        }
+        document.body.append(wrap);
+
+
+
+    })
+;
 
 
--- js/miniproect/minuproect_js.js
+++ js/miniproect/minuproect_js.js
@@ -3,6 +3,12 @@
 // 2 Вывести id,name всех user в index.html. Отдельный блок для каждого user.
 // 3 Добавить каждому блоку кнопку/ссылку , при клике на которую происходит переход на страницу user-details.html, которая имеет детальную информацию про объект на который кликнули
 
+// На странице user-details.html:
+// 4 Вывести всю, без исключения, информацию про объект user на кнопку/ссылку которого был совершен клик ранее.
+// 5 Добавить кнопку "post of current user", при клике на которую, появляются title всех постов текущего юзера
+// (для получения постов используйте эндпоинт https://jsonplaceholder.typicode.com/users/USER_ID/posts)
+//     6 Каждому посту добавить кнопку/ссылку, при клике на которую происходит переход на страницу post-details.html, которая имеет детальную информацию про текущий пост.
+
 fetch('https://jsonplaceholder.typicode.com/users')
     .then((response) => response.json())
     .then((json) => {
@@ -12,15 +18,52 @@ fetch('https://jsonplaceholder.typicode.com/users')
             let div_id = document.createElement('div');
             let btn = document.createElement('button');
 
+
             div_name.innerText = `${elements.name}`;
             div_id.innerText = `${elements.id}`;
             btn.innerText = 'click';
 
             wrap.classList.add('wrap_main');
-            btn.classList.add('btn1');
+            btn.classList.add('btn');
+
+
 
             document.body.append(wrap);
-            wrap.append(div_name, div_id,btn);
+            wrap.append(div_name, div_id, btn,);
+
+
+            btn.onClick="document.location='https://www.google.com.ua/?hl=ru'"
+
+            // btn.onclick = () => {
+            //     fetch('https://jsonplaceholder.typicode.com/users')
+            //         .then((response) => response.json())
+            //         .then((user_detailes) => {
+            //
+            //
+            //             for (const xxx of user_detailes) {
+            //
+            //                 let wrap = document.getElementsByClassName('wrap')
+            //                 let div_name = document.createElement('div');
+            //                 let div_id = document.createElement('div');
+            //                 let btn = document.createElement('button');
+            //                 let datailes_username = document.createElement('div');
+            //                 let datailes_email = document.createElement('div');
+            //                 let datailes_address = document.createElement('div');
+            //                 let datailes_street = document.createElement('div');
+            //                 let datailes_suite = document.createElement('div');
+            //                 let datailes_city = document.createElement('div');
+            //                 let datailes_zipcode = document.createElement('div');
+            //                 let datailes_geo = document.createElement('div');
+            //                 let datailes_lat = document.createElement('div');
+            //                 let datailes_ing = document.createElement('div');
+            //
+            //                 wrap.append(div_name, div_id, btn,datailes_username, datailes_email, datailes_address, datailes_street,
+            //                     datailes_suite, datailes_city, datailes_zipcode, datailes_geo, datailes_lat, datailes_ing);
+            //
+            //             }
+            //         });
+            // };
+
         }
     });
 
