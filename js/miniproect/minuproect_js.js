f8848250d3e6986af975be6668ae15af6a69a3d6
VladyslavBilokin <bilokin94@gmail.com>
2021-12-20 17:07:42 +0200

    ++

--- js/miniproect/minuproect_js.js
+++ js/miniproect/minuproect_js.js
@@ -3,6 +3,12 @@
 // 2 Вывести id,name всех user в index.html. Отдельный блок для каждого user.
 // 3 Добавить каждому блоку кнопку/ссылку , при клике на которую происходит переход на страницу user-details.html, которая имеет детальную информацию про объект на который кликнули
 
+// На странице user-details.html:
+// 4 Вывести всю, без исключения, информацию про объект user на кнопку/ссылку которого был совершен клик ранее.
+// 5 Добавить кнопку "post of current user", при клике на которую, появляются title всех постов текущего юзера
+// (для получения постов используйте эндпоинт https://jsonplaceholder.typicode.com/users/USER_ID/posts)
+//     6 Каждому посту добавить кнопку/ссылку, при клике на которую происходит переход на страницу post-details.html, которая имеет детальную информацию про текущий пост.
+
 
 fetch('https://jsonplaceholder.typicode.com/users')
     .then((response) => response.json())
@@ -14,7 +20,7 @@ fetch('https://jsonplaceholder.typicode.com/users')
             let btn = document.createElement('button');
 
 
-            div_name.innerText = `${elements.name}`;
+            div_name.innerText = `${elements.name}-`;
             div_id.innerText = `${elements.id}`;
             btn.innerText = 'click';
 
