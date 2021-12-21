bdaef97422f7061cd93c2015279d603b91165f06
VladyslavBilokin <bilokin94@gmail.com>
2021-12-21 09:56:35 +0200

    ++

--- js/miniproect/user_detailes js.js
+++ js/miniproect/user_detailes js.js
@@ -7,16 +7,19 @@ fetch('https://jsonplaceholder.typicode.com/users')
 
                 let wrap = document.createElement('wrap')
                 let user_div1 = document.createElement('div')
+                let btn2 = document.createElement('button');
 
-                wrap.classList.add('wrap1')
-                user_div1.classList.add('user_div1')
+                wrap.classList.add('wrap1');
+                user_div1.classList.add('user_div1');
+                btn2.classList.add('btn2')
 
                 user_div1.innerText = `${xxx.id}, ${xxx.name}, ${xxx.username}, ${xxx.email}, ${xxx.address.street},
                 ${xxx.address.suite}, ${xxx.address.city}, ${xxx.address.zipcode}, ${xxx.address.geo.lat},
                  ${xxx.address.geo.lng}`
+                btn2.innerText = 'post of current user';
 
                 document.body.append(wrap);
-                wrap.append(user_div1);
+                wrap.append(user_div1, btn2);
 
 
             }
